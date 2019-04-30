dfdcb317c2c6079ce7daafb7652735d7ebc0c81f
DimaKudelich <dima.kudelich2@mail.ru>
2019-04-30 11:30:58 +0300

    Update 6-1.cpp

--- 6-1.cpp
+++ 6-1.cpp
@@ -1,7 +1,7 @@
-﻿/*
+/*
 Возвратное кубическое уравнение. решение возвратного кубического уравнения (Ax^3+Bx^2+Bx+A=0)
 */
-//RCE-Returnable Cubic Equation
+
 #include<iostream>
 #include<iostream>
 #include<iomanip>
@@ -9,20 +9,58 @@
 using namespace std;
 class RCE
 {
-private:
+private: 
 	double A;
 	double B;
+	int f;
 	double x1;
 	double x2;
 	double x3;
 public:
-	RCE() :A{ 0 } , B{ 0 }{//мне кажется странным выбор 0 и "мусора" внутри х1,х2,х3. Тебя это не смущает????
+	RCE() :A{ 1 }, B{ 1 }, f{ 3 }, x1{ 0 }, x2{ 0 }, x3{ 0 }{
 		cout << "Default constructor is working" << '\n';
 	}
-	RCE( double A , double B ) :A{ A } , B{ B }{// "мусор" внутри х1,х2,х3. Тебя это не смущает????
+	RCE(double A, double B) :A{ A }, B{ B }, f{ 2 }, x1{ 0 }, x2{ 0 }, x3{ 0 }{
+		getRoots(A, B);
 		cout << "Constructor with your coefficients A,B is working" << '\n';
 	}
-	RCE( const RCE& t ) :A{ t.A } , B{ t.B } { // предыдущий "мусор" внутри х1,х2,х3. Тебя это не смущает????
+	double getRoots(double A,double B) {
+		if ((A == 0) && (B == 0)) {
+			f = 1;
+			return f;
+		}
+		else
+		{
+			if ((A == 0) && (B != 0)) {
+				f = 2;
+				x1 = 0;
+				x2 = -1;
+				return f, x1, x2;
+			}
+			else if ((B == 0) && (A != 0)) {
+				f = 3;
+				x1 = -1;
+				return f, x1;
+			}
+			else {
+				x1 = -1;
+				double D = pow((B - A), 2) - 4 * pow(A, 2);
+				double d = sqrt(D);
+				if (D > 0) {
+					f = 4;
+					x2 = (-(B - A) + d) / (2 * A);
+					x3 = (-(B - A) - d) / (2 * A);
+					return f, x1,x2,x3;
+				}
+				else if (D == 0) {
+					f = 5;
+					x2 = (-(B - A)) / (2 * A);
+					return f, x1,x2;
+				}
+			}
+		}
+	}
+	RCE(const RCE& t) :A{ t.A }, B{ t.B }, f{ t.f }, x1{ t.x1 }, x2{ t.x2 }, x3{ t.x3 } {
 		cout << "Copy constructor is working" << '\n';
 	}
 	~RCE() {
@@ -34,67 +72,46 @@ public:
 	double getB() {
 		return B;
 	}
-	RCE operator =( const RCE& e ) {
-		return RCE( e.A , e.B ); // предыдущий "мусор" внутри х1,х2,х3. Тебя это не смущает????
+	RCE operator =(const RCE& e) {
+		return RCE(e.A, e.B);
 	}
-	RCE operator +( RCE& a ) {
-		return RCE( getA() + a.getA() , getB() + a.getB() ); // предыдущий "мусор" внутри х1,х2,х3. Тебя это не смущает????
+	RCE operator +(RCE& a) {
+		return RCE(getA() + a.getA(), getB() + a.getB());
 	}
-	RCE operator -( RCE& a ) {
-		return RCE( getA() - a.getA() , getB() - a.getB() ); // предыдущий "мусор" внутри х1,х2,х3. Тебя это не смущает????
+	RCE operator -(RCE& a) {
+		return RCE(getA() - a.getA(), getB() - a.getB());
 	}
-	RCE operator *( double q ) {
-		return RCE( getA()*q , getB()*q ); // предыдущий "мусор" внутри х1,х2,х3. Тебя это не смущает????
+	RCE operator *(double q) {
+		return RCE(getA()*q, getB()*q);
 	}
-	RCE operator /( double q ) {
-		if ( q != 0 ) {
-			return RCE( getA() / q , getB() / q ); // предыдущий "мусор" внутри х1,х2,х3. Тебя это не смущает????
+	RCE operator /(double q) {
+		if (q != 0) {
+			return RCE(getA() / q, getB() / q);
 		}
 		else {
 			cout << "We can't divide by zero" << "\n";
 		}
 	}
-	void getRoots() {//public???? Зачем???? Прочитай условие задачи!!!!!
-		if ( ( A == 0 ) && ( B == 0 ) ) {
-			cout << "Infinite number of roots" << "\n";
-		}
-		else
-		{
-			if ( ( A == 0 ) && ( B != 0 ) ) {
-				x1 = 0;
-				x2 = -1;
-				cout << "x1=" << x1 << ";" << "x2=" << "x2" << ";" << "\n";
-			}
-			else if ( ( B == 0 ) && ( A != 0 ) ) {
-				x1 = -1;
-				cout << "x1=" << x1 << ";" << "\n";
-			}
-			else {
-				x1 = -1;
-				double D = pow( ( B - A ) , 2 ) - 4 * pow( A , 2 );
-				double d = sqrt( D );
-				if ( D > 0 ) {
-					x2 = ( -( B - A ) + d ) / ( 2 * A );
-					x3 = ( -( B - A ) - d ) / ( 2 * A );
-					cout << "x1=" << x1 << ";" << "x2=" << x2 << ";" << "x3=" << x3 << ";" << "\n";
-				}
-				else if ( D == 0 ) {
-					x2 = ( -( B - A ) ) / ( 2 * A );
-					cout << "x1=" << x1 << ";" << "\n";
-				}
-				else {
-					cout << "x1=" << x1 << " " << "\n";
-				}
-			}
-		}
-
-	}
-	friend ostream& operator << ( ostream & stream , const RCE& d )
+	friend ostream& operator << (ostream & stream, const RCE& d)
 	{
-		stream << d.A << ";" << d.B << '\n';
+		if (d.f == 1) {
+			stream << d.A << ";" << d.B << ";" << "Infinite number of roots"<< '\n';
+		}
+		if (d.f == 2) {
+			stream << d.A << ";" << d.B <<";" << d.x1<< ";" << d.x2 << '\n';
+		}
+		if (d.f == 3) {
+			stream << d.A << ";" << d.B << ";" << d.x1 << '\n';
+		}
+		if (d.f == 4) {
+			stream << d.A << ";" << d.B << ";" << d.x1 << ";" << d.x2 << ";" << d.x3<<'\n';
+		}
+		if (d.f == 5) {
+			stream << d.A << ";" << d.B << ";" << d.x1 << ";" << d.x2 << '\n';
+		}
 		return stream;
 	}
-	friend istream& operator >> ( istream & stream , RCE& w )
+	friend istream& operator >> (istream & stream, RCE& w)
 	{
 		stream >> w.A >> w.B;
 		return stream;
@@ -102,16 +119,18 @@ public:
 
 };
 int main() {
-	RCE p1( 1 , 2 );
-	RCE p2( p1 );
+	RCE p1(1, 2);
+	RCE p2(p1);
 	RCE p3;
+	cout << p1;
+	cout << p2;
+	cout << p3;
 	cout << p1 + p2;
 	cout << p1 - p2;
 	double tmp;
 	cin >> tmp;
 	cout << p1 * tmp;
 	cout << p1 / tmp;
-	p1.getRoots();
-	system( "pause" );
+	system("pause");
 }
 
